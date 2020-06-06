8d4b241e35833687adfd84693bd5a3b76f9b95a6
just2click <just2click@outlook.com>
2020-06-06 14:25:21 +0300

    Added some columns and rows

--- flutter_widgets/lib/app_screens/home.dart
+++ flutter_widgets/lib/app_screens/home.dart
@@ -6,20 +6,67 @@ class Home extends StatelessWidget{
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+          padding: EdgeInsets.only(left: 10.00, top: 40.0),
           alignment: Alignment.center,
           color: Colors.deepPurple,
 //          width: 200.0,
 //          height: 100.0,
           margin: EdgeInsets.only(left: 45.0, top: 45.0),
-          child: Text(
-            'Flight',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 75.0,
-              fontFamily: 'Montserrat',
-              fontWeight: FontWeight.w300
-            ),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                          'Air Canada',
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w300
+                          )
+                      )),
+                  Expanded(
+                      child: Text(
+                        'From Israel to California via Toronto',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w300
+                        ),
+                      ))
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                          'Air America',
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w300
+                          )
+                      )),
+                  Expanded(
+                      child: Text(
+                        'From London to Rome',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w300
+                        ),
+                      ))
+                ],
+              )
+            ],
           )
       )
     );
