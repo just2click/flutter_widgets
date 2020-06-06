8a8047772a205e52c3e83dcdb70ecc275320ff50
Dror <just2click@outlook.com>
2020-06-06 20:20:25 +0300

    Added basix listview (#6)

--- flutter_widgets/lib/main.dart
+++ flutter_widgets/lib/main.dart
@@ -5,6 +5,37 @@ import './app_screens/home.dart';
 void main () {
   runApp(MaterialApp(
     title: 'Exploring UI Widgets',
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(title: Text('Basic ListView'),),
+      body: getListView()
+    ),
   ));
+}
+
+Widget getListView() {
+
+  var listView = ListView(
+    children: <Widget>[
+
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text('Landscape'),
+        subtitle: Text('Beautiful view'),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint('Landscape tapped');
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_mac),
+        title: Text('MacBook'),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone_iphone),
+        title: Text('iPhone'),
+      )
+    ],
+  );
+
+  return listView;
 }
